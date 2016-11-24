b928b83c0fbbf0aef5860a8a9e2fc936dfd2c228
Coleman Warren <coleman.warren@hotmail.com>
2016-11-23 17:58:54 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,39 @@
+// A memoized program to find the nth Fibonacci number
+#include <stdio.h>
+using namespace std;
+
+const int MAX = 1000;
+const int NIL = -1;
+
+int lookup[MAX];
+
+void initialize()
+{
+	for (int i = 0; i < MAX; i++)
+		{
+			lookup[i] = NIL;
+		}
+}
+
+int fib(int n)
+{
+	if (lookup[n] == NIL)
+	{
+		if (n >= 1)
+		{
+			lookup[n] = n;
+		}
+		else
+		{
+			lookup[n] = fib(n-1) + fib(n-2);
+		}
+	}
+	return lookup[n];
+}
+
+int main() {
+	int n = 10;
+	_initialize();
+	printf("The Fibonacci number is %d ", fib(n));
+	return 0;
+}
